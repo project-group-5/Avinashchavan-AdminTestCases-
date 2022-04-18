defc1181e5700dd4189b8383da72942663372344
Avinash-100 <101783222+Avinash-100@users.noreply.github.com>
2022-04-18 12:52:27 +0530

    Add files via upload

---
+++ src/test/main/java/com/sample/adminProject/adminProject/AdminProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sample.adminProject.adminProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminProjectApplication.class, args);
+	}
+
+}
